970a220960187580708aaeb773dba6de6562fba7
ranilton <ranilton.l@escolar.ifrn.edu.br>
2024-08-02 14:05:39 -0300

    modificando

--- xsd/cardapio.py
+++ xsd/cardapio.py
@@ -10,16 +10,17 @@ id_prato = 0
 for prato in pratos:
     id_prato+=1
     categoria= prato.getAttribute ('categoria')
-    elemento_titulo = prato.getElementsByTagName('Título')[0]
-    titulo =  elemento_titulo.firstChild.nodeValue
-    print (f'{id_prato}- {titulo}')
+    elemento_nome = prato.getElementsByTagName('Título')[0]
+    nome =  elemento_nome.firstChild.nodeValue
+    print (f'{id_prato}- {nome}')
     
 
 id_opçao = int(input("Digite a opção que deseja saber mais informações:"))
 prato = pratos[id_opçao-1]
 print("---\n")
-elemento_titulo = prato.getElementsByTagName('título')[0]
-titulo = elemento_titulo.firstChild.nodeValue
+elemento_nome = prato.getElementsByTagName('título')[0]
+nome = elemento_nome.firstChild.nodeValue
 elemento_ano = prato.getElementsByTagName('ano')[0]
+ano = elemento_ano.firstChild.nodeValue
 print("Categoria:", categoria)
-print("Título:", titulo)
+print("Título:", nome)
