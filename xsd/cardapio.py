7aeaafc825a7146fe80c07d0649bbd2de568cac8
ranilton <ranilton.l@escolar.ifrn.edu.br>
2024-08-02 14:14:40 -0300

    enviando parse do cardapio

--- xsd/cardapio.py
+++ xsd/cardapio.py
@@ -1,26 +1,54 @@
 from xml.dom.minidom import parse
-dom=parse("cardapio.xml")
 
-cardapio= dom.documentElement
+# Função auxiliar para buscar elementos com namespace
+def get_elements_by_tag_name_ns(parent, ns, tag_name):
+    return parent.getElementsByTagNameNS(ns, tag_name)
 
-pratos = cardapio.getElementsByTagName('prato')
+# Carrega o XML
+dom = parse("cardapio.xml")
 
+# Define o namespace
+namespace = "http://cardapio.org"
+
+# Obtém o elemento raiz
+cardapio = dom.documentElement
+
+# Obtém todos os elementos 'prato'
+pratos = get_elements_by_tag_name_ns(cardapio, namespace, 'prato')
+
+# Inicializa o id_prato
 id_prato = 0
 
+# Itera sobre os pratos
 for prato in pratos:
-    id_prato+=1
-    categoria= prato.getAttribute ('categoria')
-    elemento_nome = prato.getElementsByTagName('Título')[0]
-    nome =  elemento_nome.firstChild.nodeValue
-    print (f'{id_prato}- {nome}')
-    
+    id_prato += 1
+    nome_elem = get_elements_by_tag_name_ns(prato, namespace, 'nome')[0]
+    nome = nome_elem.firstChild.nodeValue
+    print(f'{id_prato} - {nome}')
 
-id_opçao = int(input("Digite a opção que deseja saber mais informações:"))
-prato = pratos[id_opçao-1]
+# Solicita a opção do usuário
+id_opcao = int(input("Digite a opção que deseja saber mais informações: "))
+prato = pratos[id_opcao - 1]
 print("---\n")
-elemento_nome = prato.getElementsByTagName('título')[0]
-nome = elemento_nome.firstChild.nodeValue
-elemento_ano = prato.getElementsByTagName('ano')[0]
-ano = elemento_ano.firstChild.nodeValue
-print("Categoria:", categoria)
-print("Título:", nome)
+
+# Obtém as informações do prato selecionado
+nome_elem = get_elements_by_tag_name_ns(prato, namespace, 'nome')[0]
+nome = nome_elem.firstChild.nodeValue
+descricao_elem = get_elements_by_tag_name_ns(prato, namespace, 'descricao')[0]
+descricao = descricao_elem.firstChild.nodeValue
+ingredientes_elem = get_elements_by_tag_name_ns(prato, namespace, 'ingredientes')[0]
+ingredientes = [ing.firstChild.nodeValue for ing in get_elements_by_tag_name_ns(ingredientes_elem, namespace, 'ingrediente')]
+preco_elem = get_elements_by_tag_name_ns(prato, namespace, 'preco')[0]
+preco = preco_elem.firstChild.nodeValue
+calorias_elem = get_elements_by_tag_name_ns(prato, namespace, 'calorias')[0]
+calorias = calorias_elem.firstChild.nodeValue
+tempo_preparo_elem = get_elements_by_tag_name_ns(prato, namespace, 'tempoPreparo')[0]
+tempo_preparo = tempo_preparo_elem.firstChild.nodeValue
+
+# Imprime as informações do prato selecionado
+print("Nome:", nome)
+print("Descrição:", descricao)
+print("Ingredientes:", ", ".join(ingredientes))
+print("Preço:", preco)
+print("Calorias:", calorias)
+print("Tempo de Preparo:", tempo_preparo)
