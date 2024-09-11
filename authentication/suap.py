7623763575b7845dfab076cdf909c52575f16e63
ranilton <ranilton.l@escolar.ifrn.edu.br>
2024-09-11 16:07:35 -0300

    enviando boletim

--- authentication/suap.py
+++ authentication/suap.py
@@ -1,32 +1,29 @@
-import requests, os, json
+import requests
 from getpass import getpass
 
 api_url = "https://suap.ifrn.edu.br/api/"
+authentication_url = f"{api_url}v2/autenticacao/token/"
+user = input("Usuário: ")
+password = getpass()
 
-def auth(api_url):
-    user = input("Matrícula: ")
-    password = getpass("Senha: ")
+data = {"username":user,"password":password}
+response = requests.post(authentication_url, json=data)
+token = response.json()['access']
+headers = {"Authorization": f'Bearer {token}'}
 
-    data = {"username":user,"password":password}
+ano_letivo = input('Digite o ano letivo: ')
+periodo_letivo = input('Digite o período letivo: ')
+print('')
+boletim_url = f"{api_url}v2/minhas-informacoes/boletim/{ano_letivo}/{periodo_letivo}/"
 
-    response = requests.post(api_url+"v2/autenticacao/token/", json=data)
-    return response.json()["access"]
+response = requests.get(boletim_url, headers=headers)
 
-def boletim_request(api_url, token, ano_letivo):
-    headers = {
-        "Authorization": f'Bearer {token}'
-    }
-    response = requests.get(f"{api_url}v2/minhas-informacoes/boletim/{ano_letivo}/1", headers=headers)
-    return response.json()
-
-if (os.path.isfile("auth/suap_api/token.json")):
-    with open("auth/suap_api/token.json") as file:
-        token = json.load(file)['token']
-        response = boletim_request(api_url, token, 2023)
-        for disciplina in response:
-            print(f"{disciplina['disciplina']} - Média final: {disciplina['media_disciplina']}")  
-else:
-    with open("auth/suap_api/token.json", "w") as file:
-        token = auth(api_url)
-        data = {"token": token}
-        json.dump(data, file)
+for data in response.json():
+    print(f"{data['disciplina']}")
+    print(f"Nota etapa 1 - {data['nota_etapa_1']['nota']}")
+    print(f"Nota etapa 2 - {data['nota_etapa_2']['nota']}")
+    print(f"Nota etapa 3 - {data['nota_etapa_3']['nota']}")
+    print(f"Nota etapa 4 - {data['nota_etapa_4']['nota']}")
+    print(f"Média final - {data['media_final_disciplina']}")
+    print(f"Número de faltas - {data['numero_faltas']}")
+    print('')
